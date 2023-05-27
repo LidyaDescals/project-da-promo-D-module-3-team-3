1dcffb38e7900a48e0d5ff16d33238fc0493163e
LidyaDescals <122229711+LidyaDescals@users.noreply.github.com>
2023-05-28 00:10:21 +0200

    Update main3.py

--- scr/main3.py
+++ scr/main3.py
@@ -24,79 +24,91 @@ pd.options.display.max_columns = None
 
 
 def prediccion(month, year, weekday, workingday, holiday, weathersit, temp, hum, windspeed):
-	with open("scr/estandarizacion.pkl", "rb") as estandarizacion:
-        	scaler = pickle.load(estandarizacion)
+    with open("scr/estandarizacion.pkl", "rb") as estandarizacion:
+        scaler = pickle.load(estandarizacion)
 
-    	with open("scr/mejor_modelo.pkl", "rb") as modelo:
-        	bosque = pickle.load(modelo)
+    with open("scr/mejor_modelo.pkl", "rb") as modelo:
+        bosque = pickle.load(modelo)
 
-    	df_pred = pd.DataFrame({"month": [month], "year": [year], "weekday": [weekday], "workingday": [workingday], "holiday": [holiday], "weathersit": [weathersit], "temp": [temp], "hum": [hum], "windspeed": [windspeed]})
-	df_pred[['month', 'year', 'weekday', 'workingday', 'holiday', 'weathersit']] = df_pred[['month', 'year', 'weekday', 'workingday', 'holiday', 'weathersit']].astype("category")
-	pred_num = df_pred.select_dtypes(include=np.number)
-	X_escaladas = scaler.transform(pred_num)
-	num_estandar = pd.DataFrame(X_escaladas, columns=pred_num.columns)
-	df_pred[num_estandar.columns] = num_estandar
+    df_pred = pd.DataFrame({"month": [month], "year": [year], "weekday": [weekday], "workingday": [workingday], "holiday": [holiday], "weathersit": [weathersit], "temp": [temp], "hum": [hum], "windspeed": [windspeed]})
+    df_pred[['month', 'year', 'weekday', 'workingday', 'holiday', 'weathersit']] = df_pred[['month', 'year', 'weekday', 'workingday', 'holiday', 'weathersit']].astype("category")
+    pred_num = df_pred.select_dtypes(include=np.number)
+    X_escaladas = scaler.transform(pred_num)
+    num_estandar = pd.DataFrame(X_escaladas, columns=pred_num.columns)
+    df_pred[num_estandar.columns] = num_estandar
 
-        mapa_month = {7: 9.67, 11: 6.64, 0: 5.69, 6: 9.65, 2: 8.13, 4: 9.54, 9: 9.2, 3: 8.47, 5: 9.63, 10: 7.61, 8: 9.82, 1: 5.95}
-        mapa_year = {2018: 4, 2019: 6, 2020: 6, 2021: 6, 2022: 6, 2023: 6, 2024: 6, 2025: 6}
-        mapa_weekday = {0: 13.82, 1: 13.98, 4: 14.77, 5: 14.59, 6: 14.37, 3: 14.54, 2: 13.9}
-        mapa_holiday = {0: 4, 1: 3}
-        mapa_weathersit = {1: 4, 2: 3, 3: 1}
+    mapa_month = {7: 9.67, 11: 6.64, 0: 5.69, 6: 9.65, 2: 8.13, 4: 9.54, 9: 9.2, 3: 8.47, 5: 9.63, 10: 7.61, 8: 9.82, 1: 5.95}
+    mapa_year = {2018: 4, 2019: 6, 2020: 6, 2021: 6, 2022: 6, 2023: 6, 2024: 6, 2025: 6}
+    mapa_weekday = {0: 13.82, 1: 13.98, 4: 14.77, 5: 14.59, 6: 14.37, 3: 14.54, 2: 13.9}
+    mapa_holiday = {0: 4, 1: 3}
+    mapa_weathersit = {1: 4, 2: 3, 3: 1}
 
-        categoricas = df_pred.select_dtypes(include="category")
-        categoricas.drop(["workingday"], axis=1, inplace=True)
-	lista_mapas = [mapa_month, mapa_year, mapa_weekday, mapa_holiday, mapa_weathersit]
+    categoricas = df_pred.select_dtypes(include="category")
+    categoricas.drop(["workingday"], axis=1, inplace=True)
+    lista_mapas = [mapa_month, mapa_year, mapa_weekday, mapa_holiday, mapa_weathersit]
 
-    	for indice, col in enumerate(categoricas):
-        	df_pred[col] = df_pred[col].map(lista_mapas[indice])
+    for indice, col in enumerate(categoricas):
+        df_pred[col] = df_pred[col].map(lista_mapas[indice])
 
-    	df_pred["workingday_0"] = 1 if workingday == 0 else 0
-    	df_pred["workingday_1"] = 1 if workingday == 1 else 0
+    df_pred["workingday_0"] = 1 if workingday == 0 else 0
+    df_pred["workingday_1"] = 1 if workingday == 1 else 0
 
-    	df_pred.drop(["workingday"], axis=1, inplace=True)
+    df_pred.drop(["workingday"], axis=1, inplace=True)
 
-    	prediction = bosque.predict(df_pred)
-    	cnt = prediction[0]
-    	return cnt
+    prediction = bosque.predict(df_pred)
+    cnt = prediction[0]
+    return cnt
 
 def main():
-    	st.title("Demo de Random Forest")
+    st.title("Demo de Random Forest")
 
-    	# meses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    	# dias_semana = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    # meses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    # dias_semana = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-    	mes = st.select_slider('Mes', ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"))
+    mes = st.select_slider('Mes', ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"))
 
-    	if mes == "Enero":
-		month = 0
-    	if mes == "Febrero":
-		month = 1
-    	if mes == "Marzo":
-		month = 2
-    	if mes == "Abril":
-		month = 3
-    	if mes == "Mayo":
-		month = 4
-    	if mes == "Junio":
-		month = 5
-    	if mes == "Julio":
-		month = 6
-    	if mes == "Agosto":
-		month = 7
-    	if mes == "Septiembre":
-		month = 8
-    	if mes == "Octubre":
-		month = 9
-    	if mes == "Noviembre":
-		month = 10
-    	if mes == "Diciembre":
-		month = 11
+    if mes == "Enero":
+	month = 0
+    if mes == "Febrero":
+	month = 1
+    if mes == "Marzo":
+	month = 2
+    if mes == "Abril":
+	month = 3
+    if mes == "Mayo":
+	month = 4
+    if mes == "Junio":
+	month = 5
+    if mes == "Julio":
+	month = 6
+    if mes == "Agosto":
+	month = 7
+    if mes == "Septiembre":
+	month = 8
+    if mes == "Octubre":
+	month = 9
+    if mes == "Noviembre":
+	month = 10
+    if mes == "Diciembre":
+	month = 11
    
-    	year = st.slider("Año", 2018, 2025)
-    	weekday = st.slider("Día de la semana", 0, 6)
-    	workingday = st.selectbox("Día laborable", [0, 1])
-    	holiday = st.selectbox("Día festivo", [0, 1])
-    	weathersit = st.slider("Clima", 1, 3)
+    year = st.slider("Año", 2018, 2025)
+    weekday = st.slider("Día de la semana", 0, 6)
+    workingday = st.selectbox("Día laborable", [0, 1])
+    holiday = st.selectbox("Día festivo", [0, 1])
+    weathersit = st.slider("Clima", 1, 3)
+    temp = st.slider("Temperatura", 0, 35)
+    hum = st.slider("Humedad", 0, 100)
+    windspeed = st.slider("Velocidad del viento", 0 , 35)
+    
+
+
+    if st.button("Predecir"):
+        prediction = prediccion(month, year, weekday, workingday, holiday, weathersit, temp, hum, windspeed)
+        st.success(f"La cantidad de bicicletas alquiladas estimada es: {int(prediction)}")
+
+if __name__ == '__main__':
+    main()
     	temp = st.slider("Temperatura", 0, 35)
     	hum = st.slider("Humedad", 0, 100)
     	windspeed = st.slider("Velocidad del viento", 0 , 35)
